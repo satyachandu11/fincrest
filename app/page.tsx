0433560a179876995d8e06881bd48b5d9706aa5d
SatyaChandu <ksatyachandu@gmail.com>
2025-03-14 15:56:00 +0530

    Shadcn Setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>FinCrest</h1>
+    <div>
+      <h1>FinCrest</h1>
+      <Button>Test Button</Button>
+    </div>
+    
   );
 }
