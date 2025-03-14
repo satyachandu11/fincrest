4863c9829a92bf0e4daa8b560e784fb73e3eb209
SatyaChandu <ksatyachandu@gmail.com>
2025-03-14 18:03:31 +0530

    header and footer setup

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const inter = Inter({ subsets: ['latin']})
 
@@ -19,7 +20,15 @@ export default function RootLayout({
       <body
         className={`${inter.className}`}
       >
-        {children}
+        <Header/>
+        <main className="min-h-screen">
+          {children}
+        </main>
+        <footer className="bg-blue-50 py-12">
+          <div className="container mx-auto px-4 text-center text-gray-600">
+            <p>Made with ❤️ by Satya Chandu</p>
+          </div>
+        </footer>
       </body>
     </html>
   );
