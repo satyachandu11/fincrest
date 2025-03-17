1e75338cb169945ea4308f3944c6dc4f965104b7
SatyaChandu <ksatyachandu@gmail.com>
2025-03-17 21:42:37 +0530

    User Authentication Setup with Clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/Header";
+import { ClerkProvider } from '@clerk/nextjs'
 
 const inter = Inter({ subsets: ['latin']})
 
@@ -16,20 +17,23 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className}`}
-      >
-        <Header/>
-        <main className="min-h-screen">
-          {children}
-        </main>
-        <footer className="bg-blue-50 py-12">
-          <div className="container mx-auto px-4 text-center text-gray-600">
-            <p>Made with ❤️ by Satya Chandu</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${inter.className}`}
+        >
+          <Header />
+          <main className="min-h-screen">
+            {children}
+          </main>
+          <footer className="bg-blue-50 py-12">
+            <div className="container mx-auto px-4 text-center text-gray-600">
+              <p>Made with ❤️ by Satya Chandu</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
+
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
+    <div className="mt-40">
       <h1>FinCrest</h1>
       <Button>Test Button</Button>
     </div>
