edb31ee4b698c61c037f09c984f82a3bb73e5f3a
SatyaChandu <ksatyachandu@gmail.com>
2025-03-21 00:06:36 +0530

    Storing user data in DB

---
+++ lib/checkUser.ts
@@ -0,0 +1,54 @@
+import { currentUser } from '@clerk/nextjs/server';
+import { db } from './prisma';
+
+interface ClerkUser {
+  id: string;
+  firstName: string | null;
+  lastName: string | null;
+  imageUrl: string | null;
+  emailAddresses: { emailAddress: string }[];
+}
+
+interface LoggedInUser {
+  id: string;
+  clerkUserId: string;
+  name: string;
+  imageUrl: string | null;
+  email: string;
+}
+
+export const checkUser = async (): Promise<LoggedInUser | null> => {
+  const user = await currentUser() as ClerkUser | null;
+
+  if (!user) {
+    return null;
+  }
+
+  try {
+    const loggedInUser = await db.user.findUnique({
+      where: {
+        clerkUserId: user.id,
+      },
+    });
+
+    if (loggedInUser) {
+      return loggedInUser;
+    }
+
+    const name = `${user.firstName} ${user.lastName}`;
+
+    const newUser = await db.user.create({
+      data: {
+        clerkUserId: user.id,
+        name,
+        imageUrl: user.imageUrl,
+        email: user.emailAddresses.length > 0 ? user.emailAddresses[0].emailAddress : '',
+      },
+    });
+
+    return newUser;
+  } catch (error: any) {
+    console.error(error.message);
+    return null;
+  }
+};
