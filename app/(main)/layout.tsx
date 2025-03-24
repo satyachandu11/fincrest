2a8d9458527a97990be267fa371496be9fa78469
SatyaChandu <ksatyachandu@gmail.com>
2025-03-24 23:33:59 +0530

    added routings and some other stuffs like page not found

---
+++ app/(main)/layout.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MainLayout = ({ children }: any) => {
+  return (
+    <div className='container mx-auto my-32'>{children}</div>
+  )
+}
+
+export default MainLayout
